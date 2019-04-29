32780f78c9a331f7344024f5068725e7061ef74b
İlker Bugüner <ilker.buguner@gmail.com>
2019-04-29 13:31:01 +0300

    Solved another part from Sets and Dictionaries Advanced lab.

---
+++ 03-Sets-And-Dictionaries-Advanced/Lab/Lab - 04. Cities by Continent and Country/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab___04._Cities_by_Continent_and_Country
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int timesToRead = int.Parse(Console.ReadLine());
+
+            var dict = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < timesToRead; i++)
+            {
+                string[] info = Console.ReadLine().Split();
+
+                string continent = info[0];
+                string country = info[1];
+                string city = info[2];
+
+                if (!dict.ContainsKey(continent))
+                {
+                    dict[continent] = new Dictionary<string, List<string>>();
+                }
+
+                if (!dict[continent].ContainsKey(country))
+                {
+                    dict[continent][country] = new List<string>();
+                }
+
+                dict[continent][country].Add(city);
+            }
+
+            foreach (var continent in dict)
+            {
+                Console.WriteLine($"{continent.Key}:");
+
+                foreach (var country in continent.Value)
+                {
+                    Console.WriteLine($"  {country.Key} -> {string.Join(", ", country.Value)}");
+                }
+            }
+        }
+    }
+}
---
+++ 03-Sets-And-Dictionaries-Advanced/Lab/Lab - 05. Record Unique Names/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab___05._Record_Unique_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int timesToRead = int.Parse(Console.ReadLine());
+            HashSet<string> names = new HashSet<string>();
+
+            for (int i = 0; i < timesToRead; i++)
+            {
+                string name = Console.ReadLine();
+
+                names.Add(name);
+            }
+
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
