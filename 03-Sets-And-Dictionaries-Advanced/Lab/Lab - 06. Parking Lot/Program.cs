aff3085119cb4549a107be6ecbed5630cdefe916
İlker Bugüner <ilker.buguner@gmail.com>
2019-04-30 23:10:09 +0300

    Solved another part from Sets and Dictionaries Advanced lab.

---
+++ 03-Sets-And-Dictionaries-Advanced/Lab/Lab - 06. Parking Lot/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab___06._Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> parking = new HashSet<string>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "END")
+                {
+                    break;
+                }
+
+                string[] command = input.Split(", ");
+
+                string direction = command[0];
+                string carNum = command[1];
+
+                if (direction == "IN")
+                {
+                    parking.Add(carNum);
+                }
+                else if (direction == "OUT")
+                {
+                    parking.Remove(carNum);
+                }
+            }
+
+            if (parking.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+                foreach (var car in parking)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+        }
+    }
+}
---
+++ 03-Sets-And-Dictionaries-Advanced/Lab/Lab - 07. SoftUni Party/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab___07._SoftUni_Party
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> vipGuests = new HashSet<string>();
+            HashSet<string> normalGuests = new HashSet<string>();
+
+            while (true)
+            {
+                string guest = Console.ReadLine();
+
+                if (guest == "PARTY")
+                {
+                    break;
+                }
+
+                if (char.IsDigit(guest[0]))
+                {
+                    vipGuests.Add(guest);
+                }
+                else
+                {
+                    normalGuests.Add(guest);
+                }
+            }
+
+            int missingGuests = vipGuests.Count + normalGuests.Count;
+
+            while (true)
+            {
+                string comingGuest = Console.ReadLine();
+
+                if (comingGuest == "END")
+                {
+                    break;
+                }
+
+                if (vipGuests.Contains(comingGuest))
+                {
+                    vipGuests.Remove(comingGuest);
+                    missingGuests--;
+                }
+
+                if (normalGuests.Contains(comingGuest))
+                {
+                    normalGuests.Remove(comingGuest);
+                    missingGuests--;
+                }
+            }
+
+            Console.WriteLine(missingGuests);
+            if (vipGuests.Any())
+            {
+                foreach (var guest in vipGuests)
+                {
+                    Console.WriteLine(guest);
+                }
+            }
+            if (normalGuests.Any())
+            {
+                foreach (var guest in normalGuests)
+                {
+                    Console.WriteLine(guest);
+                }
+            }
+        }
+    }
+}
