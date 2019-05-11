c3dceb1e456291d0d080e0db4539d0cff3de9744
İlker Bugüner <ilker.buguner@gmail.com>
2019-05-11 20:52:03 +0300

    Solved part from Functional Programming lecture lab.

---
+++ 05-Functional-Programming/Lab/Lab - 02. Sum Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab___02._Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+            Action<int[]> action = x => Console.WriteLine(x.Length);
+            action += x => Console.WriteLine(x.Sum());
+
+            action(numbers);
+        }
+    }
+}
---
+++ 05-Functional-Programming/Lab/Lab - 03. Count Uppercase Words/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab___03._Count_Uppercase_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Func<string, bool> func = x => char.IsUpper(x[0]);
+
+            foreach (var item in text.Where(func))
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
