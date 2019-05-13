fbf67734e219002524b48d96d4ccdc9df1bd82e1
İlker Bugüner <ilker.buguner@gmail.com>
2019-05-13 23:51:24 +0300

    Solved last part from Functional Programming lecture lab.

---
+++ 05-Functional-Programming/Lab/Lab - 04. Add VAT/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab___04._Add_VAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double, double> adder = x => x * 1.20;
+
+            var numbers = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse).Select(adder).ToList();
+
+            Action<double> format = x => Console.WriteLine($"{x:F2}");
+
+            numbers.ForEach(format);
+        }
+    }
+}
---
+++ 05-Functional-Programming/Lab/Lab - 05. Filter By Age/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab___05._Filter_By_Age
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+
+            int timesToRead = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < timesToRead; i++)
+            {
+                string[] personInfo = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = personInfo[0];
+                int age = int.Parse(personInfo[1]);
+
+                dict[name] = age;
+            }
+
+            string condition = Console.ReadLine();
+            int ageFilter = int.Parse(Console.ReadLine());
+
+            string[] format = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Func<int, bool> filter = CreateFilter(condition, ageFilter);
+            Action<KeyValuePair<string, int>> printer = CreatePrinter(format);
+            ForEach(dict, filter, printer);
+
+
+        }
+
+        private static void ForEach(Dictionary<string, int> dict, Func<int, bool> filter, Action<KeyValuePair<string, int>> printer)
+        {
+            foreach (var kvp in dict)
+            {
+                if (filter(kvp.Value))
+                {
+                    printer(kvp);
+                }
+            }
+        }
+
+        public static Func<int, bool> CreateFilter(string condition, int ageFilter)
+        {
+            if (condition == "older")
+            {
+                return x => x >= ageFilter;
+            }
+
+            return x => x < ageFilter;
+        }
+
+        public static Action<KeyValuePair<string, int>> CreatePrinter(string[] format)
+        {
+            if (format.Length == 2)
+            {
+                return x => Console.WriteLine($"{x.Key} - {x.Value}");
+            }
+
+            if (format[0] == "name")
+            {
+                return x => Console.WriteLine($"{x.Key}");
+            }
+
+            return x => Console.WriteLine($"{x.Value}");
+        }
+    }
+}
