aeec886a28827bf52013802c381bb358f22da3f6
İlker Bugüner <ilker.buguner@gmail.com>
2019-05-20 11:29:55 +0300

    Added a part from Functional Programming lecture homework.

---
+++ 05-Functional-Programming/Homework/Exercise - 01. Action Print/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise___01._Action_Print
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            Action<string> printer = x => Console.WriteLine(x);
+
+            names.ForEach(printer);
+
+        }
+    }
+}
---
+++ 05-Functional-Programming/Homework/Exercise - 02. Knights of Honor/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise___02._Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            Action<string> printerWithSir = x => Console.WriteLine($"Sir {x}");
+
+            names.ForEach(printerWithSir);
+        }
+    }
+}
