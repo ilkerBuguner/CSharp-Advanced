b7c6f06412197738ef7bad20a5b8d49b1213fda0
İlker Bugüner <ilker.buguner@gmail.com>
2019-05-21 19:05:26 +0300

    Added part from Functional Programming Lecture Exercise.

---
+++ 05-Functional-Programming/Homework/Exercise - 03. Custom Min Function/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercise___03._Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            Func<int[], int[]> printMinNum = x =>
+            {
+                int minNum = int.MaxValue;
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (numbers[i] < minNum)
+                    {
+                        minNum = numbers[i];
+                    }
+
+                }
+
+                Console.WriteLine(minNum);
+                return x;
+            };
+
+            printMinNum(numbers);
+        }
+    }
+}
