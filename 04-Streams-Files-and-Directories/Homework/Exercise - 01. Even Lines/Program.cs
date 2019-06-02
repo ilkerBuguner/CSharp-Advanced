a255280526629129a092917bb0a7938f75016892
İlker Bugüner <ilker.buguner@gmail.com>
2019-06-02 15:39:52 +0300

    Added some exercises from Streams, Files and Directories lecture homework.

---
+++ 04-Streams-Files-and-Directories/Homework/Exercise - 01. Even Lines/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Exercise___01._Even_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var streamReader = new StreamReader(@"../../../text.txt"))
+            {
+                using (var streamWritter = new StreamWriter(@"../../../output.txt"))
+                {
+                    int counter = 0;
+                    while (true)
+                    {
+                        string line = streamReader.ReadLine();
+
+                        if (line == null)
+                        {
+                            break;
+                        }
+
+                        if (counter % 2 == 0)
+                        {
+                            var words = line.Split().Reverse()
+                                .Select(x => x.Replace("-", "@"))
+                                .Select(x => x.Replace(",", "@"))
+                                .Select(x => x.Replace(".", "@"))
+                                .Select(x => x.Replace("!", "@"))
+                                .Select(x => x.Replace("?", "@"))
+                                .ToArray();
+
+                            streamWritter.WriteLine(string.Join(" ", words));
+                        }
+                        counter++;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 04-Streams-Files-and-Directories/Homework/Exercise - 02. Line Numbers/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Exercise___02._Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var streamReader = new StreamReader(@"../../../text.txt"))
+            {
+                using (var streamWriter = new StreamWriter(@"../../../output.txt"))
+                {
+                    int linesCount = 1;
+                    while (true)
+                    {
+                        string line = streamReader.ReadLine();
+
+                        if (line == null)
+                        {
+                            break;
+                        }
+
+                        int lettersCount = 0;
+                        int punctuationsCount = 0;
+
+                        for (int i = 0; i < line.Length; i++)
+                        {
+                            char currentChar = line[i];
+                            if (char.IsLetter(currentChar))
+                            {
+                                lettersCount++;
+                            }
+                            if (char.IsPunctuation(currentChar))
+                            {
+                                punctuationsCount++;
+                            }
+                        }
+
+                        streamWriter.WriteLine($"Line {linesCount}: {line} ({lettersCount}) ({punctuationsCount})");
+                        linesCount++;
+
+                    }
+
+                }
+            }
+        }
+    }
+}
