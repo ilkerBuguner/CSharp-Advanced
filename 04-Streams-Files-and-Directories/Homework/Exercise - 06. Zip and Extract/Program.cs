fc7e32d5805050185dff4fa3290e7c8e2698a431
İlker Bugüner <ilker.buguner@gmail.com>
2019-06-04 01:57:16 +0300

    Solved one more exercise from Streams, files and directories lecture.

---
+++ 04-Streams-Files-and-Directories/Homework/Exercise - 06. Zip and Extract/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO.Compression;
+
+namespace Exercise___06._Zip_and_Extract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string picFolderPath = ".";
+            string targetPath = "../../../result.zip";
+
+            ZipFile.CreateFromDirectory(picFolderPath, targetPath);
+
+            ZipFile.ExtractToDirectory(targetPath, "../../");
+        }
+    }
+}
