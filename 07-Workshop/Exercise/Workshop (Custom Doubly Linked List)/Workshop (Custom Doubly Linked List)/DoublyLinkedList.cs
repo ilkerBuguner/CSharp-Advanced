ac74cb099f119d6effae58b38603c128bd6f44a3
İlker Bugüner <ilker.buguner@gmail.com>
2019-06-11 00:05:15 +0300

    Made Custom DoublyLinkedList from Workshop lecture.

---
+++ 07-Workshop/Exercise/Workshop (Custom Doubly Linked List)/Workshop (Custom Doubly Linked List)/DoublyLinkedList.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Workshop__Custom_Doubly_Linked_List_
+{
+    public class DoublyLinkedList
+    {
+        private class ListNode
+        {
+            public int Value { get; set; }
+            public ListNode NextNode { get; set; }
+            public ListNode PreviousNode { get; set; }
+            public ListNode(int value)
+            {
+                Value = value;
+            }
+        }
+
+        private ListNode head;
+        private ListNode tail;
+
+        public int Count { get; private set; }
+
+        public void AddFirst(int element)
+        {
+            if (Count == 0)
+            {
+                head = tail = new ListNode(element);
+            }
+            else
+            {
+                var newHead = new ListNode(element);
+                newHead.NextNode = this.head;
+                head.PreviousNode = newHead;
+                head = newHead;
+            }
+            Count++;
+        }
+
+        public void AddLast(int element)
+        {
+            if (Count == 0)
+            {
+                head = tail = new ListNode(element);
+            }
+            else
+            {
+                var newTail = new ListNode(element);
+                newTail.PreviousNode = this.tail;
+                tail.NextNode = newTail;
+                tail = newTail;
+            }
+            Count++;
+        }
+
+        public int RemoveFirst()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+
+            var firstElement = head.Value;
+            head = head.NextNode;
+            if (head != null)
+            {
+                head.PreviousNode = null;
+            }
+            else
+            {
+                tail = null;
+            }
+            Count--;
+            return firstElement;
+        }
+
+        public int RemoveLast()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+
+            var lastElement = tail.Value;
+            tail = tail.PreviousNode;
+            if (tail != null)
+            {
+                tail.NextNode = null;
+            }
+            else
+            {
+                head = null;
+            }
+            Count--;
+            return lastElement;
+        }
+
+        public void ForEach(Action<int> action)
+        {
+            var currentNode = head;
+            while (currentNode != null)
+            {
+                action(currentNode.Value);
+                currentNode = currentNode.NextNode;
+            }
+        }
+
+        public int[] ToArray()
+        {
+            int[] arr = new int[Count];
+            int counter = 0;
+            var currentNode = head;
+            while (currentNode != null)
+            {
+                arr[counter] = currentNode.Value;
+                currentNode = currentNode.NextNode;
+                counter++;
+            }
+            return arr;
+        }
+    }
+}
---
+++ 07-Workshop/Exercise/Workshop (Custom Doubly Linked List)/Workshop (Custom Doubly Linked List)/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Workshop__Custom_Doubly_Linked_List_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList list = new DoublyLinkedList();
+
+            list.AddFirst(3);
+            list.AddFirst(2);
+            list.AddFirst(1);
+            list.AddFirst(1);
+            list.AddLast(4);
+            list.RemoveLast();
+            list.RemoveFirst();
+
+
+            list.ForEach(Console.WriteLine);
+            var arr = list.ToArray();
+            Console.WriteLine(string.Join(", ", arr));
+        }
+    }
+}
