8711753f71f79dba6dfddabe164a0eb4bcbe8809
İlker Bugüner <ilker.buguner@gmail.com>
2019-06-17 00:08:49 +0300

    Solved all problems from Functional Programming lecture.

---
+++ 05-Functional-Programming/Homework/Exercise - 10. Predicate Party!/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Exercise___10._Predicate_Party_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "Party!")
+                {
+                    break;
+                }
+
+                string[] tokens = input.Split();
+                string command = tokens[0];
+
+                Func<string, bool> filter = CreateFilter(tokens);
+
+                if (command == "Remove")
+                {
+                    for (int i = 0; i < names.Count; i++)
+                    {
+                        if (filter(names[i]))
+                        {
+                            names.RemoveAt(i);
+                            i--;
+                        }
+                    }
+                }
+                if (command == "Double")
+                {
+                    for (int i = 0; i < names.Count; i++)
+                    {
+                        if (filter(names[i]))
+                        {
+                            names.Insert(i, names[i]);
+                            i++;
+                        }
+                    }
+                }
+
+
+            }
+
+            if (names.Any())
+            {
+                Console.WriteLine($"{string.Join(", ", names)} are going to the party!");
+            }
+            else
+            {
+                Console.WriteLine("Nobody is going to the party!");
+            }
+        }
+
+        public static Func<string, bool> CreateFilter(string[] tokens)
+        {
+            if (tokens[1] == "StartsWith")
+            {
+                return x => x.StartsWith(tokens[2]);
+            }
+
+            if (tokens[1] == "EndsWith")
+            {
+                return x => x.EndsWith(tokens[2]);
+            }
+
+            return x => x.Length == int.Parse(tokens[2]);
+        }
+    }
+}
---
+++ 05-Functional-Programming/Homework/Exercise - 11. The Party Reservation Filter Module/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Exercise___11._The_Party_Reservation_Filter_Module
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            List<string> filters = new List<string>();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "Print")
+                {
+                    break;
+                }
+
+                string[] tokens = command.Split(";").ToArray();
+
+                if (tokens[0] == "Add filter")
+                {
+                    filters.Add($"{tokens[1]} {tokens[2]}");
+                }
+                else if (tokens[0] == "Remove filter")
+                {
+                    filters.Remove($"{tokens[1]} {tokens[2]}");
+                }
+            }
+
+            foreach (var filter in filters)
+            {
+                string[] tokens = filter.Split(" ");
+
+                if (tokens[0] == "Starts")
+                {
+                    names = names.Where(x => !x.StartsWith(tokens[2])).ToList();
+                }
+                if (tokens[0] == "Ends")
+                {
+                    names = names.Where(x => !x.EndsWith(tokens[2])).ToList();
+                }
+                if (tokens[0] == "Length")
+                {
+                    names = names.Where(x => x.Length != int.Parse(tokens[1])).ToList();
+                }
+                if (tokens[0] == "Contains")
+                {
+                    names = names.Where(x => !x.Contains(tokens[1])).ToList();
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", names));
+        }
+    }
+}
